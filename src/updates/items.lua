f931c509060851dcd3015217a764cf06efd301e4
atanvarno <atanvarno@gmail.com>
2024-12-01 21:56:44 +0000

    Fix capsules with fuel value

--- src/updates/items.lua
+++ src/updates/items.lua
@@ -1,9 +1,19 @@
 -- Make ash the product of burning chemical fuel
---    Will update any item with `item.fuel_category == "chemical"`, including items added by other mods in their
---    data.lua stage.
---    From base mod this updates: `coal`, `nuclear-fuel`, `rocket-fuel`, `solid-fuel`, `wood`
-for _, item in pairs(data.raw.item) do
+local function add_ash_product(item)
     if item.fuel_category ~= nil and item.fuel_category == "chemical" then
-        data.raw.item[item.name].burnt_result = "atan-ash"
+        item.burnt_result = "atan-ash"
     end
+end
+
+-- Update any capsule or item with `_.fuel_category == "chemical"`, including capsules/items added by other mods in
+-- their data.lua stage.
+-- Updates [base]: `coal`, `nuclear-fuel`, `rocket-fuel`, `solid-fuel`, `wood`
+-- Updates [space-age]: `biter-egg`, `carbon`, `jellynut-seed`, `pentapod-egg`, `spoilage`, `tree-seed`, `yumako-seed`
+for _, item in pairs(data.raw["item"]) do
+    add_ash_product(item)
+end
+
+-- Updates [space-age]: `jelly`, `jellynut`, `yumako`, `yumako mash`
+for _, capsule in pairs(data.raw["capsule"]) do
+    add_ash_product(capsule)
 end
